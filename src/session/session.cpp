11498dfc5b24f7e9b954565877b052dff21bc168
robotanarchy <robotanarchy@bingo-ev.de>
2015-07-23 19:15:28 +0200

    add TODO for the entering passive mode

--- src/session/session.cpp
+++ src/session/session.cpp
@@ -12,7 +12,17 @@ string session::answer(string msg)
 	
 	if(cmd == "TYPE") return "200 WHATEVER";
 	
-	if(cmd == "PASV") return "227";
+	if(cmd == "PASV")
+	{
+		/*
+			TODO:
+				- create a data socket
+				- send a message back in the format:
+					227 Entering Passive Mode (127,0,0,1,po,rt)
+				- maybe we can put a cooler text there.
+		*/
+		return "227";
+	}
 	
 	// if(cmd == "LIST") return "150 WHY DON'T YOU WAIT FOR A DATA CONNECTION";
 	
