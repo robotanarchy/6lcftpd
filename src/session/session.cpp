04329b2f6db82ff429bf7c8dbdfb412a381d4430
robotanarchy <robotanarchy@bingo-ev.de>
2015-07-25 14:23:50 +0200

    experimented a bit with extended passive mode

--- src/session/session.cpp
+++ src/session/session.cpp
@@ -1,27 +1,52 @@
 #include "session.hpp"
 
+/*
+	Note about session types:
+		- FTP has active and passive modes. For active, the client would
+			open a port and then the server would connect to it. Doesn't
+			work behind NAT, so we won't do that.
+		- passive mode can be divided in PASV (where the server needs to
+			tell the client the IPv4 listen IP again - SDL_Net can't do
+			that!) and EPSV (extended passive, works with ipv6 too and
+			doesn't need to tell the IP again).
+		- FileZilla FTP Client only does EPSV when using IPv6 or a proxy
+			(maybe we should make a bug report).
+		- FileZilla doesn't support the EPSV answer to a PASV request
+
+*/
+
 string session::answer(string msg)
 {
 	size_t space = msg.find_first_of(' ');
 	string cmd = (space == string::npos) ? msg
 		: msg.substr(0, space);
 	
+	// simple returns
 	if(cmd == "USER") return "230 YEAH I DON'T CARE WHO YOU ARE";
-	
 	if(cmd == "PWD") return "257 \""+m_pwd+"\"";
+	if(cmd == "TYPE") return "200 WHATEVER"; // FIXME
 	
-	if(cmd == "TYPE") return "200 WHATEVER";
 	
-	if(cmd == "PASV")
+	// tell the client that we know the EPSV command. GCC throws up when
+	// writing \n211, so we merge strings instead.
+	if(cmd == "FEAT")
+	{
+		return string("211")
+			+ "\r\n EPSV"
+			+ "\r\n" + "211 End";
+	}
+	
+	// only extended passive mode is supported!
+	if(cmd == "EPSV")
 	{
 		/*
 			TODO:
 				- create a data socket
 				- send a message back in the format:
-					227 Entering Passive Mode (127,0,0,1,po,rt)
+					229 Entering Extended Passive Mode (|||6446|)
 				- maybe we can put a cooler text there.
 		*/
-		return "227";
+		return "229 Entering Extended Passive Mode (|||2111|)";
 	}
 	
 	// if(cmd == "LIST") return "150 WHY DON'T YOU WAIT FOR A DATA CONNECTION";
